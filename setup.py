c22ddab82fe597f1bf85c6470ab7b41b5a40d206
Fabian Pedregosa <fabian@fseoane.net>
2012-09-22 21:39:07 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ranking',
+    version='0.1',
+    description='Ranking algorithms',
+    author='Fabian Pedregosa',
+    author_email='fabian@fseoane.net',
+    url='',
+    py_modules=['ranksvm', 'isotron', 'isotonic_regression'],
+)
