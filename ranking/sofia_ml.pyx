005dc72c2cbac6f691939e998db980bd65d4dfa7
Fabian Pedregosa <fabian@fseoane.net>
2012-09-26 14:35:46 +0200

    more

--- ranking/sofia_ml.pyx
+++ ranking/sofia_ml.pyx
@@ -1,9 +1,11 @@
 # distutils: language = c++
 
+BUFFER_MB = 40 # default in sofia-ml
+
 cdef extern from "src/sf-data-set.h":
     cdef cppclass SfDataSet:
         SfDataSet(bool)
         SfDataSet(string, int, bool)
 
-def bla():
-    return 0
+def train(train, fit_intercept=True):
+    data = SfDataSet(train, BUFFER_MB, fit_intercept)
