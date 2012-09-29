c28234b4742d793fabcc63ba2ad5585a1d906a0c
Fabian Pedregosa <fabian@fseoane.net>
2012-09-30 01:14:31 +0200

    Fix test

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ def test_1():
     X = np.random.randn(20, 5)
     w = np.random.randn(5)
     y = np.dot(X, w)
-    w_ = train(X, y, 1., np.ones(20))
-
-    tau, _ = stats.kendalltau(y, np.dot(X, w))
+    w_ = train(X, y, 100, np.ones(20))
+    tau, _ = stats.kendalltau(y, np.dot(X, w_))
     assert np.abs(1 - tau) > 1e-3
