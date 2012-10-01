b796cdc8267ab9982be71592d7d6db8f7edffa93
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-10-01 13:59:55 +0200

    FIX : test1 with more iterations

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import numpy as np
 from scipy import stats
-from ranking import train
+from minirank import train
 
 def test_1():
     np.random.seed(0)
-    X = np.random.randn(20, 5)
+    X = np.random.randn(200, 5)
+    query_id = np.ones(len(X))
     w = np.random.randn(5)
     y = np.dot(X, w)
-    w_ = train(X, y, 100, np.ones(20))
+    w_ = train(X, y, 100, query_id, max_iter=1000000)
     tau, _ = stats.kendalltau(y, np.dot(X, w_))
     assert np.abs(1 - tau) > 1e-3
+    print tau
+
+if __name__ == '__main__':
+    test_1()
