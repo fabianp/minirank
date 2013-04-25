45ce468c556efb514585eb0fab8da824f400304a
Fabian Pedregosa <fabian@fseoane.net>
2013-04-25 20:18:30 +0200

    kind of sort of works

--- minirank/ordinal.py
+++ minirank/ordinal.py
@@ -11,7 +11,8 @@ def load_data():
     data = np.loadtxt('minirank/data/pyrim.ord')
     X = data[:, :-1]
     y = data[:, -1]
-    return X, y
+    y -= y.min()
+    return X, y.astype(np.int)
 
 def elem_a(X, theta, w):
     t = theta - X.dot(w)
@@ -44,7 +45,7 @@ def f_ineqcons(x0, X, y):
     tmp = 0.
     for i in range(len(theta0) - 1):
         tmp += np.fmax(theta0[i] - theta0[i+1], 0) ** 2
-    return tmp
+    return - tmp
 
 def f_grad(x0, X, y):
     w, theta0 = np.split(x0, [X.shape[1]])
@@ -57,13 +58,19 @@ def f_grad(x0, X, y):
     tmp = (X * tmp[:, None]).sum(0)
 
     # gradient for w2
-    tmp2 = (a * (1 - a) - b * (1 - b)) / (a - b)
+    tmp_a = (a * (1 - a)) / (a - b)
+    tmp_b = (b * (1 - b)) / (a - b)
     tmp3 = np.zeros(theta0.size)
     for i in range(y.size):
         e = np.zeros(theta0.size)
+        e1 = e.copy()
         e[y[i]] = 1.
-        tmp3 += tmp2[i] * e
-    return np.concatenate((tmp, tmp3))
+        tmp3 += tmp_a[i] * e
+        if y[i] > 0:
+            e1[y[i] - 1] = 1.
+            tmp3 -= tmp_b[i] * e1
+    #import ipdb; ipdb.set_trace()
+    return np.concatenate((tmp, - tmp3))
 
 def ordinal_logistic(X, y):
     idx = np.argsort(y)
@@ -72,21 +79,28 @@ def ordinal_logistic(X, y):
     unique_y = np.unique(y)
     for i, u in enumerate(unique_y):
         y[y==u] = i
-    x0 = np.zeros(X.shape[1] + unique_y.size)
+    x0 = np.ones(X.shape[1] + unique_y.size)
     x0[X.shape[1]:] = np.linspace(-1, 1, unique_y.size)
 
     check= optimize.check_grad(f_obj, f_grad, x0, X, y)
-    print check
     assert check < 1.
 
-    out = optimize.fmin_slsqp(f_obj, x0, args=(X, y), f_eqcons=f_eqcons, f_ieqcons=f_ineqcons, fprime=f_grad)
+    out = optimize.fmin_slsqp(f_obj, x0, args=(X, y), f_eqcons=f_eqcons, f_ieqcons=f_ineqcons, fprime=f_grad, iter=10000)
     w, theta = np.split(out, [X.shape[1]])
     #import ipdb; ipdb.set_trace()
     return w, theta[y]
 
+def predict(w, theta, X):
+    unique_theta = np.unique(theta)
+    out = []
+    for i in range(X.shape[0]):
+        tmp = X[i].dot(w)
+        tmp2 = np.abs(unique_theta - tmp)
+        min_ = np.argsort(tmp2)[0]
+        out.append(min_)
+    return np.array(out)
 
 if __name__ == '__main__':
     X, y = load_data()
     w, theta = ordinal_logistic(X, y)
-    print w
-    print theta
+    print 'Score %s' % ((predict(w, theta, X) == y).sum() / float(y.size))
