3c00fa166c02dee20c30d900d8e6130ec6dc4525
Fabian Pedregosa <fabian@fseoane.net>
2013-04-25 19:35:29 +0200

    more work

--- minirank/ordinal.py
+++ minirank/ordinal.py
@@ -15,23 +15,55 @@ def load_data():
 
 def elem_a(X, theta, w):
     t = theta - X.dot(w)
-    return 1. / (1 + np.exp(t))
+    return 1. / (1 + np.exp(-t))
 
 def elem_b(X, theta, w):
-    _theta = np.roll(theta, 1)
-    _theta[0] = - np.inf
-    t = theta - X.dot(w)
-    return 1. / (1 + np.exp(t))
+    _theta = theta.copy()
+    unique_theta = np.unique(theta)
+    for i in range(len(unique_theta) - 1):
+        t1 = unique_theta[i]
+        t2 = unique_theta[i+1]
+        _theta[_theta == t2] = t1
+    _theta[theta == unique_theta[0]] = - np.inf
+    t = _theta - X.dot(w)
+    return 1. / (1 + np.exp(-t))
 
 def f_obj(x0, X, y):
     w, theta0 = np.split(x0, [X.shape[1]])
     theta = theta0[y]
-    tmp2 = np.sum(elem_a(X, theta, w) - elem_b(X, theta, w))
-    tmp2 += BIG * ((theta[0] + 1) ** 2)
-    tmp2 += BIG * ((theta[-1] - 1) ** 2)
-    for i in range(theta0.size - 1):
-        tmp2 += BIG * (np.fmax(theta[i] - theta[i+1], 0) ** 2)
+    tmp = elem_a(X, theta, w) - elem_b(X, theta, w)
+    tmp2 = - np.sum(np.log(tmp))
     return tmp2
+
+def f_eqcons(x0, X, y):
+    w, theta0 = np.split(x0, [X.shape[1]])
+    return (theta0[0] + 1.) ** 2 + (theta0[-1] - 1.) ** 2
+
+def f_ineqcons(x0, X, y):
+    w, theta0 = np.split(x0, [X.shape[1]])
+    tmp = 0.
+    for i in range(len(theta0) - 1):
+        tmp += np.fmax(theta0[i] - theta0[i+1], 0) ** 2
+    return tmp
+
+def f_grad(x0, X, y):
+    w, theta0 = np.split(x0, [X.shape[1]])
+
+    # gradient for w
+    theta = theta0[y]
+    a = elem_a(X, theta, w)
+    b = elem_b(X, theta, w)
+    tmp = (a * (1 - a) - b * (1 - b)) / (a - b)
+    tmp = (X * tmp[:, None]).sum(0)
+
+    # gradient for w2
+    tmp2 = (a * (1 - a) - b * (1 - b)) / (a - b)
+    tmp3 = np.zeros(theta0.size)
+    for i in range(y.size):
+        e = np.zeros(theta0.size)
+        e[y[i]] = 1.
+        tmp3 += tmp2[i] * e
+    return np.concatenate((tmp, tmp3))
 
 def ordinal_logistic(X, y):
     idx = np.argsort(y)
@@ -41,8 +73,15 @@ def ordinal_logistic(X, y):
     for i, u in enumerate(unique_y):
         y[y==u] = i
     x0 = np.zeros(X.shape[1] + unique_y.size)
-    out = optimize.minimize(f_obj, x0, args=(X, y))
-    w, theta = np.split(out.x, [X.shape[1]])
+    x0[X.shape[1]:] = np.linspace(-1, 1, unique_y.size)
+
+    check= optimize.check_grad(f_obj, f_grad, x0, X, y)
+    print check
+    assert check < 1.
+
+    out = optimize.fmin_slsqp(f_obj, x0, args=(X, y), f_eqcons=f_eqcons, f_ieqcons=f_ineqcons, fprime=f_grad)
+    w, theta = np.split(out, [X.shape[1]])
+    #import ipdb; ipdb.set_trace()
     return w, theta[y]
 
 
