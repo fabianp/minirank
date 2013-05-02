29b3a33d4df7c928cfcb7afee5698b01559c51eb
Fabian Pedregosa <fabian@fseoane.net>
2013-05-02 16:14:52 +0200

    cosmetic

--- minirank/logistic.py
+++ minirank/logistic.py
@@ -1,3 +1,7 @@
+"""
+Implementation of logistic ordinal regression (aka proportional odds) model
+"""
+
 from __future__ import print_function
 
 from sklearn import utils, metrics
@@ -5,22 +9,6 @@ from scipy import linalg, optimize, sparse
 import numpy as np
 
 BIG = 1e10
-
-def f_ineqcons(x0, X, y):
-    w, theta0 = np.split(x0, [X.shape[1]])
-    out = np.diff(theta0)
-    return out
-
-def f_ineqcons_grad(x0, X, y):
-    w, theta0 = np.split(x0, [X.shape[1]])
-    e1 = np.eye(theta0.size, 1).ravel()
-    e2 = e1.copy()
-    e2[1] = -1.
-    L = - linalg.toeplitz(e1, e2)
-    L = L[:-1]
-    T = np.zeros((L.shape[0], x0.size))
-    T[:, x0.size - L.shape[1]:] = L[:, :]
-    return -T
 
 
 def ordinal_logistic(X, y, max_iter=1000, verbose=False):
@@ -30,13 +18,21 @@ def ordinal_logistic(X, y, max_iter=1000, verbose=False):
 
     Parameters
     ----------
-
-    X : {array, sparse matrix}
-
+    X : {array, sparse matrix}, shape (n_samples, n_feaures)
+        Input data
     y : array-like
-
+        Target values
     max_iter : int
-        maximum number of iterations
+        Maximum number of iterations
+    verbose: bool
+        Print convergence information
+
+    Returns
+    -------
+    w : array, shape (n_features,)
+        coefficients of the linear model
+    theta : array, shape (k,), where k is the different values of y
+        vector of thresholds
     """
 
     X = utils.safe_asarray(X)
@@ -100,16 +96,29 @@ def ordinal_logistic(X, y, max_iter=1000, verbose=False):
 
         return np.concatenate((grad_w, grad_theta))
 
+    def f_ineqcons(x0, X, y):
+        w, theta0 = np.split(x0, [X.shape[1]])
+        out = np.diff(theta0)
+        return out
+
+    def f_ineqcons_grad(x0, X, y):
+        w, theta0 = np.split(x0, [X.shape[1]])
+        e1 = np.eye(theta0.size, 1).ravel()
+        e2 = e1.copy()
+        e2[1] = -1.
+        L = - linalg.toeplitz(e1, e2)
+        L = L[:-1]
+        T = np.zeros((L.shape[0], x0.size))
+        T[:, x0.size - L.shape[1]:] = L[:, :]
+        return -T
 
     x0 = np.ones(X.shape[1] + unique_y.size) / X.shape[0]
-    x0[X.shape[1]:] = np.linspace(-1, 1, unique_y.size)
+    x0[X.shape[1]:] = np.linspace(-.9, .9, unique_y.size)
 
     if False:
-        # check gradient is correctly computed
+        # check that gradient is correctly computed
         check = optimize.check_grad(f_obj, f_grad, x0, X, y)
-        print(optimize.approx_fprime(x0, f_obj, 1e-6, X, y))
-        print(f_grad(x0, X, y))
-        assert check < 1.
+        assert check / f_obj(x0, X, y) < 1.
 
     bounds = [(-BIG, BIG)] * X.shape[1] + [(-1, 1)] * unique_y.size
 
@@ -120,56 +129,69 @@ def ordinal_logistic(X, y, max_iter=1000, verbose=False):
     return w, theta[y][idx_inv]
 
 
-def predict_logistic(w, theta, X):
+def predict_logistic(w, theta, X, y):
+    """
+    Parameters
+    ----------
+    w : coefficients obtained by ordinal_logistic
+    theta : thresholds
+    """
+    unique_y = np.sort(np.unique(y))
     unique_theta = np.unique(theta)
-    # import ipdb; ipdb.set_trace()
     mu = [-1]
     for i in range(unique_theta.size - 1):
         mu.append((unique_theta[i] + unique_theta[i+1]) / 2.)
         # todo: use roll
-
     out = X.dot(w)
     mu = np.array(mu)
     tmp = metrics.pairwise.pairwise_distances(out[:, None], mu[:, None])
-    return np.argmin(tmp, 1)
-
-def load_data():
-    data = np.loadtxt('minirank/data/pyrim.ord')
-    X = data[:, :-1]
-    X -= X.mean()
-    y = data[:, -1]
-    y -= y.min()
-    return X, y
+    return unique_y[np.argmin(tmp, 1)]
 
 if __name__ == '__main__':
-    from sklearn import cross_validation
-    X, y = load_data()
+    from sklearn import cross_validation, datasets
+    boston = datasets.load_boston()
+    X, y = boston.data, np.round(boston.target)
+    X -= X.mean()
+    y -= y.min()
+
     idx = np.argsort(y)
     X = X[idx]
     y = y[idx]
-    cv = cross_validation.StratifiedShuffleSplit(y, n_iter=50, test_size=.25, random_state=0)
+    cv = cross_validation.ShuffleSplit(y.size, n_iter=50, test_size=.1, random_state=0)
     score_logistic = []
     score_ordinal_logistic = []
+    score_ridge = []
     for i, (train, test) in enumerate(cv):
+        if not np.all(np.unique(y[train]) == np.unique(y)):
+            # we need the train set to have all different classes
+            continue
         assert np.all(np.unique(y[train]) == np.unique(y))
         train = np.sort(train)
         test = np.sort(test)
         w, theta = ordinal_logistic(X[train], y[train])
-        pred = predict_logistic(w, theta, X[test])
-        s = ((pred == y[test]).sum() / float(test.size))
-        print('Score (ORDINAL)  fold %s: %s' % (i+1, s))
+        pred = predict_logistic(w, theta, X[test], y)
+        s = metrics.mean_absolute_error(y[test], pred)
+        print('ERROR (ORDINAL)  fold %s: %s' % (i+1, s))
         score_ordinal_logistic.append(s)
 
         from sklearn import linear_model
-        clf = linear_model.LogisticRegression(C=1e10)
+        clf = linear_model.LogisticRegression(C=1.)
         clf.fit(X[train], y[train])
         pred = clf.predict(X[test])
-        s = ((pred == y[test]).sum() / float(test.size))
-        print('Score (LOGISTIC) fold %s: %s' % (i+1, s))
+        s = metrics.mean_absolute_error(y[test], pred)
+        print('ERROR (LOGISTIC) fold %s: %s' % (i+1, s))
         score_logistic.append(s)
 
-    print()
-    print('MEAN SCORE (ORDINAL LOGISTIC):    %s' % np.mean(score_ordinal_logistic))
-    print('MEAN SCORE (LOGISTIC REGRESSION): %s' % np.mean(score_logistic))
-    print('Chance level is at %s' % (1. / np.unique(y).size))
+        from sklearn import linear_model
+        clf = linear_model.Ridge(alpha=1.)
+        clf.fit(X[train], y[train])
+        pred = np.round(clf.predict(X[test]))
+        s = metrics.mean_absolute_error(y[test], pred)
+        print('ERROR (LOGISTIC) fold %s: %s' % (i+1, s))
+        score_ridge.append(s)
 
+
+    print()
+    print('MEAN ABSOLUTE ERROR (ORDINAL LOGISTIC):    %s' % np.mean(score_ordinal_logistic))
+    print('MEAN ABSOLUTE ERROR (LOGISTIC REGRESSION): %s' % np.mean(score_logistic))
+    # print('Chance level is at %s' % (1. / np.unique(y).size))
